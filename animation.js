a30999ae4127083944ceabd1d225acb671b2e0ba
Taylor Wong <Taylor Wong>
2018-03-12 05:54:21 -0400

    uh

--- animation.js
+++ animation.js
@@ -6,95 +6,108 @@ var width = svg.getAttribute("width");
 // buttons
 var clear = document.getElementById("clear");
 
-var isRunning = false;
+//var isRunning = false;
 
 var circles = [];
 
 // clear screen
 var clearCallBack = function(){
-  window.cancelAnimationFrame(id);
+  var curr = 0
+  for (curr = 0; circles.length > curr; curr += 1) {
+    clearInterval(circles[curr]);
+  }
+
   while(svg.firstChild){
 	   svg.removeChild(svg.firstChild);
   }
 };
 
 // circle object
-var circle = function(x, y, r, c){
-  var c = {
-    circle: document.createElementNS("http://www.w3.org/2000/svg", "circle"),
-    dx : 3,
-  	dy : 4,
-  	getX: function(){
-  	    return this.circle.getAttribute("cx");
-  	},
-  	getY: function(){
-  	    return this.circle.getAttribute("cy");
-  	},
-  	getRadius: function(){
-  	    return this.circle.getAttribute("r");
-  	},
-    getColor: function(){
-  	    return this.circle.getAttribute("fill");
-  	},
-    getDx: function(){
-  	    return this.dx;
-  	},
-  	getDy: function(){
-  	    return this.dy;
-  	},
-  	setX: function(x1){
-  	    this.circle.setAttribute("cx", x1);
-  	},
-  	setY: function(y1){
-  	    this.circle.setAttribute("cy", y1);
-  	},
-  	setRadius: function(r1){
-  	    this.circle.setAttribute("r", r1);
-  	},
-    setColor: function(c1){
-  	    this.circle.setAttribute("fill", c1);
-        this.circle.setAttribute("stroke", "black");
-  	},
-  	setDx: function(dx){
-  	    this.dx = dx;
-  	},
-  	setDy: function(dy){
-  	    this.dy = dy;
-  	},
-    move: function(){
-      if (this.getX() <= this.getRadius() || this.getX() >= width - this.getRadius()) {
-        this.setDx(getDx() * -1);
+var circle = function(x, y, r, col, dx, dy){
+  var c = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+
+
+    //accessor functions
+  	c.getX = function(){
+  	    return this.getAttribute("cx");
+        console.log('get x');
+  	};
+  	c.getY = function(){
+  	    return this.getAttribute("cy");
+        console.log('get y');
+  	};
+  	c.getRadius = function(){
+  	    return this.getAttribute("r");
+        console.log('get rad');
+  	};
+    c.getColor = function(){
+  	    return this.getAttribute("fill");
+        console.log('get col');
+  	};
+
+    //mutator functions
+  	c.setX = function(x1){
+  	    this.setAttribute("cx", x1);
+        console.log('set y');
+  	};
+  	c.setY = function(y1){
+  	    this.setAttribute("cy", y1);
+        console.log('set x');
+  	};
+  	c.setRadius = function(r1){
+  	    this.setAttribute("r", r1);
+        console.log('set rad');
+  	};
+    c.setColor = function(c1){
+  	    this.setAttribute("fill", c1);
+        this.setAttribute("stroke", "black");
+        console.log('set color');
+  	};
+
+    //display function --YES
+    c.myDisplay = function(){
+      svg.appendChild(this);
+      console.log('displayed circle');
+  	};
+
+    //move function--YES
+    c.move = function(){
+      if (c.getX() <= c.getRadius() || c.getX() >= width - c.getRadius()) {
+        dx = dx * -1;
       }
-      if (this.getY() <= this.getRadius() || this.getY() >= height - this.getRadius()) {
-        this.setDy(getDy() * -1);
+      if (c.getY() <= c.getRadius() || c.getY() >= height - c.getRadius()) {
+        dy = dy * -1;
       }
-      this.setX(this.getX() + this.getDx());
-      this.setY(this.getY() + this.getDy());
-    },
-    display: function(){
-      svg.appendChild(this.circle);
-  	}
-  }
+      //new coor
+      c.setX(c.getX() + dx);
+      c.setY(c.getY() + dy);
+      console.log('x: ' + c.getX());
+      console.log('y: ' + c.getY());
+      //show
+      c.myDisplay();
+
+      console.log('moving');
+    };
+
+  //attributes
   c.setX(x);
   c.setY(y);
   c.setRadius(r);
-  c.setColor(c);
+  c.setColor(col);
+
+  var newCircle = setInterval(c.move, 20);
+  circles.push(newCircle);
   return c;
+  console.log('new circle made');
 };
 
-var animate = function(){
-  circles.forEach(
-    function(c){
-      c.move();
-    }
-  )
-}
 
 var svgCallBack = function(){
-  var c = circle(Math.random() * width, Math.random() * height, 10,"blue");
-  c.display();
-  circles.push(c);
-  id = window.requestAnimationFrame(animate);
+  var c = circle(Math.random() * width, Math.random() * height, 10,"blue", 3, 4);
+  console.log('svgcallback');
+  //c.myDisplay();
+  //circles.push(c);
+  //id = window.requestAnimationFrame(animate);
 };
 
 clear.addEventListener("click", clearCallBack);
